dc331e432e59e9d315a78568acfeffb0f789af2b
Uriel Aguirre <uriel.flores1410@gmail.com>
2024-10-11 20:17:12 -0600

    prueba api

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,4 +1,21 @@
 package Principal;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Principal {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/6095e66f549e686a5f419354/latest/USD"))
+                .build();
+
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+    }
 }
